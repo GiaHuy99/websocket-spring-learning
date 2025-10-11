4f74b2023aef77b4946af0aa31e09f2ce3d85f8c
huy1412 <huytgse170167@fpt.edu.vn>
2025-10-11 14:35:48 +0700

    update

--- Lesson4/src/main/java/giahuypro/lesson4/SimpleController.java
+++ Lesson4/src/main/java/giahuypro/lesson4/SimpleController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class SimpleController {
-    @MessageMapping("/chat")  // Route /app/send
-    @SendTo("/topic/chat")   // Broadcast /topic/reply
+    @MessageMapping("/reply")  // Route /app/send
+    @SendTo("/topic/chat")   // Broadcast /topic/chat
     public String handleSimple(String message) {
         System.out.println("Backend nhận message: " + message);  // Log debug
         return "Echo đơn giản: " + message;  // Handle và broadcast
